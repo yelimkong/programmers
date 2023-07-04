03d204cd53773e3aabc9fd067f05bc198316aa88
yelimkong <127725222+yelimkong@users.noreply.github.com>
2023-07-04 21:47:29 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -0,0 +1,11 @@
+def solution(angle):
+    result = 0
+    if 0 < angle < 90 :
+        result = 1
+    elif angle == 90 :
+        result = 2
+    elif 90 < angle < 180 :
+        result = 3
+    else :
+        result = 4
+    return result
