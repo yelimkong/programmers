e391b31f88a256c383e1b74322c18c13bd4228ce
yelimkong <127725222+yelimkong@users.noreply.github.com>
2023-07-18 01:05:20 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = []
+    for num in range(n+1) :
+        if num % 2 != 0 :
+            answer.append(num)
+            pass
+        else : 
+            pass
+    return answer
