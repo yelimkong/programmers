8aef1ee7020d9ca94bcb06a2afd013ecffbfe79b
yelimkong <127725222+yelimkong@users.noreply.github.com>
2023-11-02 12:58:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,7 +1,8 @@
 import math
+
 def solution(n):
     answer = 0
-    if math.sqrt(n)  == int(n**0.5) :
-        return math.pow((math.sqrt(n) + 1), 2)
-    else : 
-        return -1
+    if math.sqrt(n)  == int(n**0.5) : # n의 제곱근이 n에 루트 씌운 것과 같다면
+        return math.pow((math.sqrt(n) + 1), 2) # n의 제곱근 + 1을 제곱하고
+    else : # 제곱근이 존재하지 않는다면
+        return -1 # -1을 반환
